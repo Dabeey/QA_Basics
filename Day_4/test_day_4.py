0d086e54b659de99519873c9ada7c0951e1bd256
Dabeey <abbasdaughter18@gmail.com>
2025-07-28 21:32:47 +0100

    Add pytest tests for addition, hello, and sample list functionality

--- Day_4/test_day_4.py
+++ Day_4/test_day_4.py
@@ -4,12 +4,17 @@ def test_addition():
     assert 2 + 2 == 4, "Addition test failed" #if the addition is not correct
     print("Addition test passed successfully.") #if the addition is correct
 
+def test_hello():
+    assert "Hello" == "Hello", "Hello test failed"
+    print("Hello test passed successfully.")
+
 
 @pytest.fixture
 def sample_list():
     print("🔧 Setting up sample list")
     yield [1,2,3,4]
     print("🧹 Cleaning up after test!")
+    
 
 def test_sample_list(sample_list):
     assert sum(sample_list) == 10, "Sum of sample list should be 10"
@@ -17,7 +22,7 @@ def test_sample_list(sample_list):
     assert len(sample_list) == 4, "Length of sample list should be 4"
     print("Length of sample list test passed successfully.")
 
-
+# Multiple tests in a class
 class TestMath:
     def test_add(self):
         assert 1 + 1 == 2
