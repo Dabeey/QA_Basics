66563507461891dc3cae292f4da8666c99cd39a5
Dabeey <abbasdaughter18@gmail.com>
2025-08-05 12:55:34 +0100

    Add API testing functions for user and login endpoints

--- Day_10/test.py
+++ Day_10/test.py
@@ -1,7 +1,31 @@
 import requests
 
-URL = ''
+URL = 'https://reqres.in/api'
 
 def test_url():
-    res = requests.get(URL)
-    assert res.status
+    res = requests.get(URL + '/users')
+    assert res.status_code == 200, f'Expected 200 as status code'
+    print(f'Test passed: {res.status_code} received from {URL}')
+    return res.json()
+
+
+def test_api():
+    res = requests.get(URL + '/users/4')
+    assert res.json() is not None, 'Expected a JSON response'
+    print(f'Test passed: JSON response received from {URL}')
+    return res.json()
+
+
+def test_login():
+    payload = {
+        "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
+    }
+
+    res = requests.post(URL + '/login',json=payload)
+    assert res.status_code == 200, f'Expected 200 as status code'
+    assert 'token' in res.json(), 'Expected a token in the response'
+    data = res.json()
+    print(f'Test passed: Login successful with token {data["token"]}')
+    return data
+
