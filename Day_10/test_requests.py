502d45c35a35f8a3f741ac5cdb5fc8565c2eb22a
Dabeey <abbasdaughter18@gmail.com>
2025-08-05 21:56:08 +0100

    Add .gitignore file

--- Day_10/test_requests.py
+++ Day_10/test_requests.py
@@ -1,19 +1,28 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-URL = 'https://reqres.in/api'
+load_dotenv()
+URL = os.getenv('API_URL')
 
 def test_url():
     res = requests.get(URL + '/users')
     assert res.status_code == 200, f'Expected 200 as status code'
     print(f'Test passed: {res.status_code} received from {URL}')
-    return res.json()
+    data = res.json()
+    assert isinstance(data, dict), 'Expected a JSON response'
+    assert 'data' in data, 'Expected "data" key in the response'
+    return data
 
 
 def test_api():
     res = requests.get(URL + '/users/4')
     assert res.json() is not None, 'Expected a JSON response'
     print(f'Test passed: JSON response received from {URL}')
-    return res.json()
+    data = res.json()
+    assert isinstance(data, dict), 'Expected a JSON response'
+    # assert 'data' in data, 'Expected "data" key in the response'
+    return data
 
 
 def test_login():
@@ -23,7 +32,7 @@ def test_login():
     }
 
     res = requests.post(URL + '/login',json=payload,
-                        headers={'x-api-key':'reqres-free-v1'})
+                        headers={'x-api-key': os.getenv('X_API_KEY')})
     print(f'Status Code: {res.status_code}')
     assert res.status_code == 200, f'Expected 200 as status code'
     assert 'token' in res.json(), 'Expected a token in the response'
